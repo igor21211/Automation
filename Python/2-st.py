7e4547b366dd51415e4760a45f1f26cb89c0cc22
igor <spuraigor@gmail.com>
2021-09-30 20:58:09 +0300

    Pa

--- Python/2-st.py
+++ Python/2-st.py
@@ -1,6 +1,30 @@
 x = 15
-y = 4.5
+y = 54
 
 
 
 print(type(y))
+
+#Bool
+My_true = True
+my_false = False
+y = int(y)
+print(y)
+print(type(y))
+y = bool(y)
+print(type(y))
+print(y)
+
+word = ''' \
+I wanna be your slave
+i wanna be your killer
+Just give me your hand
+just do this together
+'''
+# print(word)
+#
+# verse = 'its rain like\n' \
+#         'Cat and dog'
+# print(verse)
+
+print(word[::-1])
