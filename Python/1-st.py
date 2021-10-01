b16ee36e50de71bcc5dccb0f47ef25b51a704bee
igor <spuraigor@gmail.com>
2021-10-01 23:52:07 +0300

    Lessons1_7

--- Python/1-st.py
+++ Python/1-st.py
@@ -1,7 +1,38 @@
-print(5 + 10)
-print(5 - 2)
-print(5 * 5)
-print(10 / 2)
-print(40 % 6)
-print(5 ** 4)
-print(10 // 5)
+# print(5 + 10)
+# print(5 - 2)
+# print(5 * 5)
+# print(10 / 2)
+# print(40 % 6)
+# print(5 ** 4)
+# print(10 // 5)
+# name = 'Igor'
+# age = 30
+# salary = 3500
+# # print('My name is {0}, I am {1}, and i want have salary {2}'.format(name,age,salary))
+# # print('My name is {x}, I am {y}, and i want have salary {w}'.format(x=name,y=age,w=salary))
+# # print(f'My name is {name}, I am {age} , and i want have salary {salary}')
+# # print('453+234={}'.format(453+234))
+# # print(f'453+234={453+234}')
+# price = int(input('Сколько зарплаты хочешь?'))
+#
+# if salary>price:
+#     print(f'Нет, это мало, я щас зарабатываю {salary}, а вы мне предлагаете на {salary-price} меньше')
+# elif salary == price:
+#     print('Накиньте немног')
+# else:
+#     print(f'Я согласен, это больше на {price-salary} чем моя нынешняя зарплата')
+#
+#
+# time = int(input('Которий час?'))
+#
+# if not time:
+#     print('True')
+# else:
+#     print('False')
+
+i = 1
+
+while i<=10:
+    print(i, end=' ')
+    i+=1
+
--- Python/2-st.py
+++ Python/2-st.py
@@ -1,30 +1,43 @@
-x = 15
-y = 54
+# x = 15
+# y = 54
 
 
 
-print(type(y))
+# print(type(y))
+#
+# #Bool
+# My_true = True
+# my_false = False
+# y = int(y)
+# print(y)
+# print(type(y))
+# y = bool(y)
+# print(type(y))
+# print(y)
 
-#Bool
-My_true = True
-my_false = False
-y = int(y)
-print(y)
-print(type(y))
-y = bool(y)
-print(type(y))
-print(y)
-
-word = ''' \
-I wanna be your slave
-i wanna be your killer
-Just give me your hand
-just do this together
-'''
+# # word = ''' \
+# I wanna be your slave
+# i wanna be your killer
+# Just give me your hand
+# just do this together
+# '''
 # print(word)
 #
 # verse = 'its rain like\n' \
 #         'Cat and dog'
 # print(verse)
-
-print(word[::-1])
+# s2 = len(word)
+# s = 'babybe'
+# print(s.capitalize())
+# print(len(word))
+# print(s.center(25,'$'))
+# print(word.count('i'))
+# print(word.count('i'))
+# print(word.find('u'))
+# print(word.index('m'))
+# print(s.index('g'))
+# print(s.replace('e','a'))
+# print(word.split())
+# print(word.isdigit())
+# print(word.isalpha())
+# print(s.isalnum())
